ee54167b107ded9dad8dfdad7d0add0102c456b7
TPeterW <peterwangtao0@hotmail.com>
2017-05-06 21:33:54 -0400

    Search page

--- renderer.js
+++ renderer.js
@@ -1,3 +1,33 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let axios = require('axios');
+
+const apiUrl = (term) => {
+	return `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC`;
+};
+
+let searchBar = document.getElementById('search');
+searchBar.onkeypress = (e) => {
+	if (e.keyCode === 13) {
+		e.preventDefault();
+
+		if (searchBar.value === "")
+			return;
+
+		// loading
+		document.getElementById('img-show')
+				.setAttribute('src', 'imgs/loading.gif');
+
+		axios.get(apiUrl(searchBar.value), {})
+			.then((res) => {
+				document.getElementById('img-show')
+						.setAttribute('src', res.data.data[0].images.original.url);
+			}).catch((err) => {
+				document.getElementById('img-show')
+						.setAttribute('src', "imgs/error.png");
+				console.log(err);
+			});
+	}
+};
